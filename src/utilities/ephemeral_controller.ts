30f9d0a0cbd96cd8b7068c62c251451840b3c500
Sub-Xaero <Sub-Xaero@users.noreply.github.com>
2021-01-21 01:57:24 +0000

    Utility Base-Controller - Ephemeral Controller. Adds cleanup methods to remove all the values associated with a controller.

---
+++ src/utilities/ephemeral_controller.ts
@@ -0,0 +1,45 @@
+import camelCase from "lodash.camelcase";
+import {Controller} from "stimulus";
+
+export class EphemeralController extends Controller {
+
+  cleanupSelf() {
+    this.cleanup(this.element as HTMLElement);
+  }
+
+  cleanup(element: HTMLElement) {
+    element.dataset.controller = element.dataset.controller?.replaceAll(new RegExp(`(\s|^)${this.identifier}(\s|$)`, "g"), "") || "";
+    if (element.dataset.controller == "") {
+      // If there are no controllers left, remove the attribute
+      delete element.dataset.controller;
+    }
+
+    let substringIdentifierValueRegex = new RegExp(`(\s|^)${this.identifier}\\..+?(\s|$)`, "g");
+    element.dataset.target = element.dataset.target?.replaceAll(substringIdentifierValueRegex, "") || "";
+    delete element.dataset[camelCase(`${this.identifier}-target`)];
+    if (element.dataset.target == "") {
+      // If there are no targets left, remove the attribute
+      delete element.dataset.target;
+    }
+
+    element.dataset.action = element.dataset.target?.replaceAll(substringIdentifierValueRegex, "") || "";
+    delete element.dataset[camelCase(`${this.identifier}-action`)];
+    if (element.dataset.action == "") {
+      // If there are no actions left, remove the attribute
+      delete element.dataset.action;
+    }
+
+    // @ts-ignore
+    let values = this.constructor.values;
+    if (values) {
+      Object.keys(values).forEach(val => delete element.dataset[camelCase(`${this.identifier}-${val}-value`)]);
+    }
+
+    // @ts-ignore
+    let classes = this.constructor.classes;
+    if (classes) {
+      Object.keys(classes).forEach(val => delete element.dataset[camelCase(`${this.identifier}-${val}-class`)]);
+    }
+  }
+
+}
