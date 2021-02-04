3a87c0e236a5cd1b3c9944ec7a539e9842fe55eb
Sub-Xaero <Sub-Xaero@users.noreply.github.com>
2021-02-04 00:50:58 +0000

    Dependencies - Use lodash instead of lodash-es. Lodash-es seems to have maintenance issues

--- src/utilities/ephemeral_controller.ts
+++ src/utilities/ephemeral_controller.ts
@@ -1,4 +1,4 @@
-import camelCase from "lodash.camelcase";
+import camelCase from "lodash/camelCase";
 import {BaseController} from "../base_controller";
 
 export class EphemeralController extends BaseController {
