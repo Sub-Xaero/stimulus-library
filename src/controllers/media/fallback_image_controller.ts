1fd682dc7185891217e97da5f66e14afb4679ed9
Sub-Xaero <Sub-Xaero@users.noreply.github.com>
2021-04-28 11:41:10 +0100

    feat: FallbackImageController - update the controller to support failing when the placeholder also fails. Also works more consistently in race-conditions when the image has already loaded when the controller mounts.

--- src/controllers/media/fallback_image_controller.ts
+++ src/controllers/media/fallback_image_controller.ts
@@ -7,16 +7,35 @@ export class FallbackImageController extends BaseController {
   declare readonly placeholderValue: string;
   declare readonly hasPlaceholderValue: boolean;
 
+  initialize() {
+    this._loadedSuccessfully = this._loadedSuccessfully.bind(this);
+    this._fail = this._fail.bind(this);
+  }
+
   connect() {
     let element = this.el as HTMLImageElement;
 
-    element.onerror = () => {
-      if (this.hasPlaceholderValue) {
-        element.src = this.placeholderValue;
-      } else {
-        element.style.display = "none";
-      }
-    };
+    element.onerror = this._fail;
+    if (element.complete && !this._loadedSuccessfully()) {
+      this._fail();
+    }
   }
 
+  private _fail() {
+    let element = this.el as HTMLImageElement;
+
+    if (this.hasPlaceholderValue && element.src !== this.placeholderValue) {
+      this.dispatch(element, "fallback-image:placeholder");
+      element.src = this.placeholderValue;
+      element.onerror = this._fail;
+    } else {
+      this.dispatch(element, "fallback-image:fail");
+      element.style.display = "none";
+    }
+  }
+
+  private _loadedSuccessfully(): boolean {
+    let element = this.el as HTMLImageElement;
+    return element.naturalHeight > 0 && element.naturalWidth > 0;
+  }
 }
