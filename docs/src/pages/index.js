e99833fbde9df8846507c6f1c10bf3b65d2625f9
Dylan Clarke <sub-xaero@users.noreply.github.com>
2021-07-02 11:18:54 +0100

    chore(docs): Update docs homepage

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -12,36 +12,37 @@ const features = [
     title: "Vanilla",
     description: (
       <>
-        We don't like bloat, so wherever possible we ship Stimulus-Library with zero third party dependencies. You won't find controllers here that simply wrap up other JS libraries like date-pickers. There are other libraries if that's what you want.
+        Nobody likes bloat, so wherever possible Stimulus-Library ships with zero third party dependencies. You won't find controllers here that simply wrap up other JS libraries like date-pickers. There are other libraries if that's what you want.
         <br/>
         <br/>
-        We specialise in simple, vanilla controllers that do one simple thing. </>
+        Stimulus-Library is just simple, vanilla controllers that do one simple thing.
+      </>
     ),
   },
   {
     title: "Configurable",
     description: (
       <>
-        While Stimulus Library sets sensible defaults for all controllers, we appreciate that not all use-cases are the same. We've built all of our controllers to to be as flexible and configurable as possible to enable your application. Found a use-case we didn't consider? Open a Github issue and let us know! </>
+        While Stimulus Library sets sensible defaults for all controllers, not all use-cases are the same. All of our controllers are built to to be as flexible and configurable as possible to enable your application. Found a use-case we didn't consider? Open a Github issue and let us know!
+      </>
     ),
   },
   {
     title: "Comprehensive",
     description: (
       <>
-        Here at Stimulus Library, we hate installing 50 different node packages just to get 50 different Javascript *Sprinkles*. We provide you with a comprehensive set of lightweight controllers with the hope that when you find yourself needing a new behaviour, that we already have a controller that does it for you, without you having to install umpteen packages.
+        Hate installing ~50 different node packages just to get ~50 different Javascript ✨Sprinkles✨? We provide you with a comprehensive set of lightweight controllers with the hope that when you find yourself needing a new behaviour, that we already have a controller that does it for you, without you having to install more packages.
         <br/>
         <br/>
-        The library is fully tree-shakable, so install Stimulus-Library guilt free - only the controllers that you use are bundled into your application. </>
+        The library is fully tree-shakable, so install Stimulus-Library guilt free - only the controllers that you use are bundled into your application.
+      </>
     ),
   },
   {
     title: "Fire and Forget",
     description: (
       <>
-        While all controllers in Stimulus-Library are composable and provide a comprehensive set of events that you can use to wire together multiple controllers -
-
-        for simple things like autosizing textareas, you shouldn't have to remember to wire up the various DOM events to the corresponding Stimulus action, you should be able to just drop it in and forget about it.
+        While all controllers in Stimulus-Library are composable and provide a comprehensive set of events that you can use to wire together multiple controllers - for simple things like auto-sizing text-areas, you shouldn't have to remember to wire up the various DOM events to the corresponding Stimulus action, you should be able to just drop it in and forget about it.
         <br/>
         <br/>
         Stimulus-Library, where it seems sensible, creates controllers that you can just "Fire and Forget". If you don't like that, you can turn it off and wire things up yourself. You're the boss. </>
