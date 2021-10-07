c2663bbb787b7980cb0b159b38d0aee68db7bae3
Dylan Clarke <sub-xaero@users.noreply.github.com>
2021-10-07 11:36:32 +0100

    feat: RemoteFormController - Add an event for `remote-form:replace` to signify a replace has occurred

--- src/controllers/forms/remote_form_controller.ts
+++ src/controllers/forms/remote_form_controller.ts
@@ -15,9 +15,9 @@ export class RemoteFormController extends BaseController {
   replace(event: { detail: [Element, any, XMLHttpRequest] }) {
     const [data, status, xhr] = event.detail;
     if (data instanceof Node) {
-      let new_element = data.querySelector(this._selector);
+      let newElement: HTMLElement | null = data.querySelector(this._selector);
 
-      if (new_element == null) {
+      if (newElement == null) {
         throw new Error(`expected new form DOM with [data-controller="${this.identifier}"] to be present in returned payload`);
       }
 
@@ -25,7 +25,8 @@ export class RemoteFormController extends BaseController {
       if (parentNode == null) {
         throw new Error('expected form to have a DOM parent, could not execute replaceChild');
       }
-      parentNode.replaceChild(new_element, this.el);
+      parentNode.replaceChild(newElement, this.el);
+      this.dispatch(newElement, 'remote-form:replace');
     } else {
       console.log('Unknown', data);
     }
