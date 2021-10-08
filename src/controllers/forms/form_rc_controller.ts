d7c4ed4dd613071f510057ad3e98283c25132878
Dylan Clarke <sub-xaero@users.noreply.github.com>
2021-10-08 10:26:30 +0100

    feat: Allow FormRCController to submit a form found either via a CSS selector or via a target

--- src/controllers/forms/form_rc_controller.ts
+++ src/controllers/forms/form_rc_controller.ts
@@ -4,17 +4,34 @@ import {requestReset, requestSubmit} from "../../utilities/requestSubmit";
 export class FormRcController extends BaseController {
 
   static targets = ["form"];
+  static values = { formSelector: String};
 
   declare readonly formTarget: HTMLFormElement;
 
+  declare readonly formSelectorValue: string;
+  declare readonly hasFormSelectorValue: boolean;
+
+  get form(): HTMLFormElement {
+    let form: HTMLFormElement | null;
+    if (this.hasFormSelectorValue) {
+      form = document.querySelector(this.formSelectorValue);
+      if (!form) {
+        throw new Error(`Could not find a form on the page that matches selector '${this.formSelectorValue}'`)
+      }
+    } else {
+      form = this.formTarget;
+    }
+    return form;
+  }
+
   submit(event?: Event) {
     event?.preventDefault();
-    requestSubmit(this.formTarget);
+    requestSubmit(this.form);
   }
 
   reset(event?: Event) {
     event?.preventDefault();
-    requestReset(this.formTarget)
+    requestReset(this.form)
   }
 
 }
