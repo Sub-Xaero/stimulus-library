a069d7bf7f16e13db431dce1e98a96768238b26b
Dylan Clarke <sub-xaero@users.noreply.github.com>
2021-10-21 16:44:49 +0100

    feat: PresenceController - Added an optional name value which if provided will be included in the presence event name so that other controllers can react to specific presence events if there are multiple in a given scope.

--- src/controllers/utility/presence_controller.ts
+++ src/controllers/utility/presence_controller.ts
@@ -2,12 +2,21 @@ import {BaseController} from "../../utilities/base_controller";
 
 export class PresenceController extends BaseController {
 
+  static values = {name: String};
+
+  declare nameValue: string;
+  declare readonly hasNameValue: boolean;
+
+  get name(): string {
+    return this.hasNameValue ? `${this.nameValue}:` : ``;
+  }
+
   connect() {
-    this.dispatch(this.el, "presence:added");
+    this.dispatch(this.el, `${this.nameValue}presence:added`);
   }
 
   disconnect() {
-    this.dispatch(this.el, "presence:removed");
+    this.dispatch(this.el, `${this.nameValue}presence:removed`);
   }
 
 }
