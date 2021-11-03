7c057f1efef48f848e77736415b9dc82a87a8738
Dylan Clarke <sub-xaero@users.noreply.github.com>
2021-11-03 13:55:21 +0000

    feat: Add submitMode value to FormRCController to allow changing between calling .submit and .requestSubmit

--- src/controllers/forms/form_rc_controller.ts
+++ src/controllers/forms/form_rc_controller.ts
@@ -4,19 +4,34 @@ import {requestReset, requestSubmit} from "../../utilities/request_submit";
 export class FormRcController extends BaseController {
 
   static targets = ["form"];
-  static values = { formSelector: String};
+  static values = {
+    formSelector: String,
+    submitMode: String,
+  };
 
+  declare submitModeValue: "direct" | "request";
+  declare readonly hasSubmitModeValue: boolean;
   declare readonly formTarget: HTMLFormElement;
-
   declare readonly formSelectorValue: string;
   declare readonly hasFormSelectorValue: boolean;
+
+  get _mode(): "direct" | "request" {
+    if (this.hasSubmitModeValue) {
+      if (!["direct", "request"].includes(this.submitModeValue)) {
+        throw new Error(`The modeValue provided '${this.submitModeValue}' is not one of the recognised configuration options`);
+      }
+      return this.submitModeValue;
+    } else {
+      return "request";
+    }
+  }
 
   get form(): HTMLFormElement {
     let form: HTMLFormElement | null;
     if (this.hasFormSelectorValue) {
       form = document.querySelector(this.formSelectorValue);
       if (!form) {
-        throw new Error(`Could not find a form on the page that matches selector '${this.formSelectorValue}'`)
+        throw new Error(`Could not find a form on the page that matches selector '${this.formSelectorValue}'`);
       }
     } else {
       form = this.formTarget;
@@ -26,12 +41,17 @@ export class FormRcController extends BaseController {
 
   submit(event?: Event) {
     event?.preventDefault();
-    requestSubmit(this.form);
+    let el = this.el as HTMLFormElement;
+    if (this._mode == 'request') {
+      requestSubmit(el);
+    } else {
+      el.submit();
+    }
   }
 
   reset(event?: Event) {
     event?.preventDefault();
-    requestReset(this.form)
+    requestReset(this.form);
   }
 
 }
