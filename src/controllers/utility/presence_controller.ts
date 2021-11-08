0ab40117e9289cc127b73f0010c90fede5f70fb0
Dylan Clarke <sub-xaero@users.noreply.github.com>
2021-11-08 18:06:37 +0000

    fix: PresenceController - Fix event name not matching documentation

--- src/controllers/utility/presence_controller.ts
+++ src/controllers/utility/presence_controller.ts
@@ -8,15 +8,23 @@ export class PresenceController extends BaseController {
   declare readonly hasNameValue: boolean;
 
   get name(): string {
-    return this.hasNameValue ? `${this.nameValue}:` : ``;
+    return this.hasNameValue ? this.nameValue : ``;
+  }
+
+  get _addedEventName(): string {
+    return [this.name, 'presence', 'added'].join(':');
+  }
+
+  get _removedEventName(): string {
+    return [this.name, 'presence', 'removed'].join(':');
   }
 
   connect() {
-    this.dispatch(this.el, `${this.nameValue}presence:added`);
+    this.dispatch(this.el, this._addedEventName);
   }
 
   disconnect() {
-    this.dispatch(this.el, `${this.nameValue}presence:removed`);
+    this.dispatch(this.el, this._removedEventName);
   }
 
 }
