09bad647b776118d4946fb5a6a57b516bf660c50
Dylan Clarke <Sub-Xaero@users.noreply.github.com>
2021-11-12 01:36:23 +0000

    chore: Throw error in PollBlockController when seconds value not present

--- src/controllers/ajax/poll_block_controller.ts
+++ src/controllers/ajax/poll_block_controller.ts
@@ -6,9 +6,14 @@ export class PollBlockController extends LoadBlockController {
   static targets = ["replace"];
   static values = {endpoint: String, errorMessage: String, selector: String, seconds: Number};
 
+  declare readonly hasSecondsValue: boolean;
   declare readonly secondsValue: number;
 
   connect() {
+    if (!this.hasSecondsValue) {
+      throw new Error("Required value `seconds` is missing");
+    }
+
     requestAnimationFrame(() => {
       this._timeout();
       useInterval(this, this._timeout, this.secondsValue * 1000);
