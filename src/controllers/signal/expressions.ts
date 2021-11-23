786c327b940417d465554428d2f3584d2e78844f
Dylan Clarke <Sub-Xaero@users.noreply.github.com>
2021-11-23 10:57:12 +0000

    refactor: Extract signal expression parsing to own file

---
+++ src/controllers/signal/expressions.ts
@@ -0,0 +1,55 @@
+export function extractPredicates(expressionString: string): Array<(val: string | number) => boolean> {
+  expressionString = expressionString.replaceAll(" ", "").trim();
+
+  let andExpression = expressionString.includes("&&");
+  let orExpression = expressionString.includes("||");
+  let groupings = expressionString.includes("(") || expressionString.includes(")");
+
+  if (andExpression && orExpression) {
+    throw new Error("Cannot have both && and || in the same expression.");
+  }
+
+  if (groupings) {
+    throw new Error("Cannot have logical groupings `(>3 && <= 9) || (>1 && <2)` in the expression. Only supports simple expressions like  `>1 && <3`");
+  }
+
+  let expressions = expressionString.split(andExpression ? "&&" : "||");
+  if (andExpression) {
+    return expressions.map(ex => _predicateForExpression(ex));
+  } else if (orExpression) {
+    return expressions.map(ex => _predicateForExpression(ex));
+  } else {
+    return [_predicateForExpression(expressionString)];
+  }
+}
+
+
+function _predicateForExpression(expression: string): (val: string | number) => boolean {
+  let operators = [">=", "<=", "==", "!=", ">", "<"];
+  let operator = operators.find(part => expression.includes(part));
+  if (!operator) {
+    throw new Error(`Could not find operator in expression: ${expression}`);
+  }
+  let expressionValue: string | number = expression.split(operator)[1];
+  let isNumber = /^-?[0-9]\d*(\.\d+)?$/.test(expressionValue);
+  expressionValue = isNumber ? parseFloat(expressionValue) : expressionValue;
+
+  let notEmpty = (signalValue: string | number) => signalValue !== "";
+
+  switch (operator) {
+    case ">":
+      return (signalValue: string | number) => isNumber && notEmpty(signalValue) && signalValue > expressionValue;
+    case "<":
+      return (signalValue: string | number) => isNumber && notEmpty(signalValue) && signalValue < expressionValue;
+    case ">=":
+      return (signalValue: string | number) => isNumber && notEmpty(signalValue) && signalValue >= expressionValue;
+    case "<=":
+      return (signalValue: string | number) => isNumber && notEmpty(signalValue) && signalValue <= expressionValue;
+    case "==":
+      return (signalValue: string | number) => notEmpty(signalValue) && signalValue == expressionValue;
+    case "!=":
+      return (signalValue: string | number) => notEmpty(signalValue) && signalValue != expressionValue;
+    default:
+      throw new Error(`Unknown operator ${operator}`);
+  }
+}
--- src/controllers/signal/signal_visibility_controller.ts
+++ src/controllers/signal/signal_visibility_controller.ts
@@ -1,6 +1,7 @@
 import {BaseController} from "../../utilities/base_controller";
 import {EventBus} from "../../utilities/event_bus";
 import {SignalPayload} from "./signal_input_controller";
+import {extractPredicates} from "./expressions";
 
 export class SignalVisibilityController extends BaseController {
 
@@ -17,27 +18,7 @@ export class SignalVisibilityController extends BaseController {
   declare readonly hasHideClass: boolean;
 
   get _predicates(): Array<(val: string | number) => boolean> {
-    let expressionString = this.showValue.replaceAll(" ", "").trim();
-
-    let andExpression = expressionString.includes("&&");
-    let orExpression = expressionString.includes("||");
-    let groupings = expressionString.includes("(") || expressionString.includes(")");
-
-    if (andExpression && orExpression) {
-      throw new Error("Cannot have both && and || in the same expression.");
-    }
-    if (groupings) {
-      throw new Error("Cannot have logical groupings `(>3 && <= 9) || (>1 && <2)` in the expression. Only supports simple expressions like  `>1 && <3`");
-    }
-
-    let expressions = expressionString.split(andExpression ? "&&" : "||");
-    if (andExpression) {
-      return expressions.map(ex => this._predicateForExpression(ex));
-    } else if (orExpression) {
-      return expressions.map(ex => this._predicateForExpression(ex));
-    } else {
-      return [this._predicateForExpression(expressionString)];
-    }
+    return extractPredicates(this.showValue);
   }
 
   get _hideClasses(): string[] {
@@ -50,36 +31,6 @@ export class SignalVisibilityController extends BaseController {
 
   get _eventName(): string {
     return `signal:value:${this.nameValue}`;
-  }
-
-  _predicateForExpression(expression: string): (val: string | number) => boolean {
-    let operators = [">=", "<=", "==", "!=", ">", "<"];
-    let operator = operators.find(part => expression.includes(part));
-    if (!operator) {
-      throw new Error(`Could not find operator in expression: ${expression}`);
-    }
-    let expressionValue: string | number = expression.split(operator)[1];
-    let isNumber = /^-?[0-9]\d*(\.\d+)?$/.test(expressionValue);
-    expressionValue = isNumber ? parseFloat(expressionValue) : expressionValue;
-
-    let notEmpty = (signalValue: string | number) => signalValue !== "";
-
-    switch (operator) {
-      case ">":
-        return (signalValue: string | number) => isNumber && notEmpty(signalValue) && signalValue > expressionValue;
-      case "<":
-        return (signalValue: string | number) => isNumber && notEmpty(signalValue) && signalValue < expressionValue;
-      case ">=":
-        return (signalValue: string | number) => isNumber && notEmpty(signalValue) && signalValue >= expressionValue;
-      case "<=":
-        return (signalValue: string | number) => isNumber && notEmpty(signalValue) && signalValue <= expressionValue;
-      case "==":
-        return (signalValue: string | number) => notEmpty(signalValue) && signalValue == expressionValue;
-      case "!=":
-        return (signalValue: string | number) => notEmpty(signalValue) && signalValue != expressionValue;
-      default:
-        throw new Error(`Unknown operator ${operator}`);
-    }
   }
 
   initialize() {
