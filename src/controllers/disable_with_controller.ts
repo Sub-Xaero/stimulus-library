8f1d254c78a1691b0e55ee4307030e37d2466789
Dylan Clarke <Sub-Xaero@users.noreply.github.com>
2022-01-18 17:51:35 +0000

    feat(Mixins): New mixin useTemporaryContent for setting content of an element temporarily and restoring it either on demand or after a set time

--- src/controllers/disable_with_controller.ts
+++ src/controllers/disable_with_controller.ts
@@ -1,7 +1,7 @@
 import {BaseController} from '../utilities/base_controller';
-import {isHTMLAnchorElement, isHTMLButtonElement, isHTMLInputElement} from "../utilities/elements";
+import {isTypeOfButtonableElement} from "../utilities/elements";
 import {useEventListener} from "../mixins/use_event_listener";
-import {useTimeout} from "../mixins/use_timeout";
+import {useTemporaryContent} from "../mixins/use_temporary_content";
 
 export class DisableWithController extends BaseController {
 
@@ -14,8 +14,6 @@ export class DisableWithController extends BaseController {
   declare readonly hasMessageValue: boolean;
   declare readonly timeoutValue: number;
   declare readonly hasTimeoutValue: boolean;
-
-  _cacheText?: string;
 
   get _message(): string {
     return this.hasMessageValue ? this.messageValue : 'Submitting...';
@@ -39,10 +37,8 @@ export class DisableWithController extends BaseController {
       event?.preventDefault();
       event?.stopImmediatePropagation();
     } else {
-      this._cacheText = this._getElText(element);
-      this._setElText(element, this._message);
       this._disable();
-      useTimeout(this, this.enable, this._timeout);
+      useTemporaryContent(this, element, this._message, this._timeout, this._enable);
     }
   }
 
@@ -50,38 +46,21 @@ export class DisableWithController extends BaseController {
     event?.preventDefault();
     let element = this.el;
     if (this._isDisabled(element)) {
-      this._setElText(element, this._cacheText!);
       this._enable();
     }
   }
 
   _isDisabled(el: HTMLElement): boolean {
-    if ((isHTMLInputElement(el) && el.type == "submit") || isHTMLButtonElement(el)) {
+    if (isTypeOfButtonableElement(el)) {
       return el.disabled;
     } else {
       return el.dataset.disabled == "true";
     }
   }
 
-  _getElText(el: HTMLElement): string {
-    if (isHTMLInputElement(el) && el.type == "submit") {
-      return el.value;
-    } else {
-      return el.innerText;
-    }
-  }
-
-  _setElText(el: HTMLElement, str: string) {
-    if (isHTMLInputElement(el) && el.type == "submit") {
-      el.value = str;
-    } else if (isHTMLButtonElement(el) || isHTMLAnchorElement(el)) {
-      el.innerText = str;
-    }
-  }
-
   _disable() {
     let el = this.el;
-    if ((isHTMLInputElement(el) && el.type == "submit") || isHTMLButtonElement(el)) {
+    if (isTypeOfButtonableElement(el)) {
       el.disabled = true;
     } else {
       el.dataset.disabled = "true";
@@ -90,7 +69,7 @@ export class DisableWithController extends BaseController {
 
   _enable() {
     let el = this.el;
-    if (isHTMLInputElement(el) && el.type == "submit" || isHTMLButtonElement(el)) {
+    if (isTypeOfButtonableElement(el)) {
       el.disabled = false;
     } else {
       el.dataset.disabled = undefined;
