2253a685bd0f58aebd6c5075159c3f592becfd81
Dylan Clarke <Sub-Xaero@users.noreply.github.com>
2022-02-01 12:41:12 +0000

    test: Fix LazyBlockController spec

--- cypress/integration/lazy_block_controller.spec.js
+++ cypress/integration/lazy_block_controller.spec.js
@@ -1,11 +1,10 @@
 describe('Lazy Block Controller', () => {
   beforeEach(() => {
     cy.visit('controllers/lazy_block_controller.html');
+  });
 
-    it('Should contain remote content', () => {
-      cy.get('#output').contains('This content came from another page');
-    });
-
+  it('Should contain remote content', () => {
+    cy.get('#output').contains('This content came from another page');
   });
 
 });
