07aa80686579650242998c2376b807bb870b3f78
Dylan Clarke <Sub-Xaero@users.noreply.github.com>
2022-02-16 13:38:51 +0000

    fix: Ensure that dispatched events always have the target in the details obj

--- src/utilities/events.ts
+++ src/utilities/events.ts
@@ -3,7 +3,7 @@ import {logEvent} from "./logging";
 
 export function dispatchEvent(controller: Controller, element: HTMLElement, eventName: string, options: CustomEventInit = {}) {
   let mergedOptions = Object.assign({}, {bubbles: true, cancelable: true, detail: {target: element}}, options);
-  if (!!mergedOptions.detail.target) {
+  if (!mergedOptions.detail.target) {
     mergedOptions.detail.target = element;
   }
   let event = new CustomEvent(eventName, mergedOptions);
