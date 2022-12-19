Sub-Xaero/stimulus-library

commit 2430d07ac60c06d25a9e50f67c47919fbb536e0a

chore(refactor): Introduce a new Signal base class that DRYs up some repeated code and adds some helper methods

src/controllers/signal/signal_action_controller.ts
@@ -1,11 +1,8 @@
-import {BaseController} from "../../utilities/base_controller";
 import {SignalPayload} from "./signal_input_controller";
-import {extractPredicates} from "./expressions";
-import {useEventBus} from "../../mixins/use_event_bus";
-import {EventBus} from "../../utilities";
-import {signalConnectEvent, signalEventName, signalValueEvent} from "./events";
+import {signalEventName} from "./events";
+import {SignalBaseController} from "./base_controller";
 
-export class SignalActionController extends BaseController {
+export class SignalActionController extends SignalBaseController {
 
   static values = {
     name: String,
@@ -17,27 +14,17 @@ export class SignalActionController extends BaseController {
 
   declare nameValue: string;
 
-  get _predicates(): Array<(val: string | number) => boolean> {
-    return extractPredicates(this.whenValue);
-  }
-
-  connect() {
-    EventBus.emit(signalConnectEvent(this.nameValue));
-    useEventBus(this, signalValueEvent(this.nameValue), this._onSignal);
-  }
-
   _onSignal(payload: SignalPayload) {
     let value = payload.value;
     if (!this.hasWhenValue) {
       this.dispatchEvent(this.el, signalEventName(this.nameValue, 'match'));
       return;
     }
-    if (this._predicates.every(predicate => predicate(value))) {
+    if (this.allPredicatesMatch(value)) {
       this.dispatchEvent(this.el, signalEventName(this.nameValue, 'match'), {detail: {element: this.el, value}});
     } else {
       this.dispatchEvent(this.el, signalEventName(this.nameValue, 'no-match'), {detail: {element: this.el, value}});
     }
   }
-
 
 }

src/controllers/signal/base_controller.ts
@@ -1,0 +1,34 @@
+import {BaseController} from "../../utilities/base_controller";
+import {SignalPayload} from "./signal_input_controller";
+import {extractPredicates} from "./expressions";
+import {EventBus} from "src/*";
+import {signalConnectEvent, signalValueEvent} from "./events";
+import {useEventBus} from "../../mixins/use_event_bus";
+
+export abstract class SignalBaseController extends BaseController {
+
+  static values = {
+    name: String,
+  };
+
+  declare nameValue: string;
+
+  connect() {
+    EventBus.emit(signalConnectEvent(this.nameValue));
+    useEventBus(this, signalValueEvent(this.nameValue), this._onSignal);
+  }
+
+  get predicateString() {
+    return "";
+  }
+
+  get _predicates(): Array<(val: string | number) => boolean> {
+    return extractPredicates(this.predicateString);
+  }
+
+  allPredicatesMatch(value: SignalPayload['value']): boolean {
+    return this._predicates.every(predicate => predicate(value));
+  }
+
+  abstract _onSignal(payload: SignalPayload): void;
+}

src/controllers/signal/signal_visibility_controller.ts
@@ -1,12 +1,8 @@
-import {BaseController} from "../../utilities/base_controller";
-import {useEventBus} from "../../mixins/use_event_bus";
 import {SignalPayload} from "./signal_input_controller";
-import {extractPredicates} from "./expressions";
-import {signalConnectEvent, signalValueEvent, signalVisibilityEvent} from "./events";
-import {EventBus} from "../../utilities";
-import {installClassMethods} from "../../mixins/install_class_methods";
+import {signalVisibilityEvent} from "./events";
+import {SignalBaseController} from "./base_controller";
 
-export class SignalVisibilityController extends BaseController {
+export class SignalVisibilityController extends SignalBaseController {
 
   static values = {
     name: String,
@@ -20,18 +16,8 @@ export class SignalVisibilityController extends BaseController {
   declare addHideClasses: (el?: HTMLElement) => void;
   declare removeHideClasses: (el?: HTMLElement) => void;
 
-  get _predicates(): Array<(val: string | number) => boolean> {
-    return extractPredicates(this.showValue);
-  }
-
   get defaultHideClasses(): string[] {
     return ["hide"];
-  }
-
-  connect() {
-    installClassMethods(this);
-    EventBus.emit(signalConnectEvent(this.nameValue));
-    useEventBus(this, signalValueEvent(this.nameValue), this._onSignal);
   }
 
   _onSignal(payload: SignalPayload) {
@@ -44,7 +30,7 @@ export class SignalVisibilityController extends BaseController {
       }
       return;
     }
-    if (this._predicates.every(predicate => predicate(value))) {
+    if (this.allPredicatesMatch(value)) {
       this.dispatchEvent(this.el, signalVisibilityEvent(this.nameValue, "show"), {detail: {predicate: this.showValue, value}});
       this.removeHideClasses(this.el);
     } else {
@@ -52,6 +38,5 @@ export class SignalVisibilityController extends BaseController {
       this.addHideClasses(this.el);
     }
   }
-
 
 }

commit 9bc878c80bee51d195db701e0bba99ed6322941e

fix(Controller): Fix bug in Signal controllers preventing controllers seeing any expressions. Regression introduced in 2430d07ac60c06d25a9e50f67c47919fbb536e0a

src/controllers/signal/signal_action_controller.ts
@@ -14,6 +14,10 @@ export class SignalActionController extends SignalBaseController {
 
   declare nameValue: string;
 
+  get predicateString() {
+    return this.whenValue;
+  }
+
   _onSignal(payload: SignalPayload) {
     let value = payload.value;
     if (!this.hasWhenValue) {

src/controllers/signal/signal_disable_controller.ts
@@ -12,6 +12,10 @@ export class SignalDisableController extends SignalBaseController {
   declare nameValue: string;
   declare whenValue: string;
 
+  get predicateString() {
+    return this.whenValue;
+  }
+
   disable() {
     this.el.setAttribute("disabled", "true");
   }
@@ -31,10 +35,18 @@ export class SignalDisableController extends SignalBaseController {
       return;
     }
     if (this.allPredicatesMatch(value)) {
-      this.dispatchEvent(this.el, signalEventName(this.nameValue, "disable"), {detail: {predicate: this.whenValue, value}});
+      this.dispatchEvent(this.el, signalEventName(this.nameValue, "disable"), {
+        detail: {
+          predicate: this.whenValue, value
+        }
+      });
       this.disable();
     } else {
-      this.dispatchEvent(this.el, signalEventName(this.nameValue, "enable"), {detail: {predicate: this.whenValue, value}});
+      this.dispatchEvent(this.el, signalEventName(this.nameValue, "enable"), {
+        detail: {
+          predicate: this.whenValue, value
+        }
+      });
       this.enable();
     }
   }

src/controllers/signal/signal_enable_controller.ts
@@ -12,6 +12,10 @@ export class SignalEnableController extends SignalBaseController {
   declare nameValue: string;
   declare whenValue: string;
 
+  get predicateString() {
+    return this.whenValue;
+  }
+
   disable() {
     this.el.setAttribute("disabled", "true");
   }
@@ -31,10 +35,20 @@ export class SignalEnableController extends SignalBaseController {
       return;
     }
     if (this.allPredicatesMatch(value)) {
-      this.dispatchEvent(this.el, signalEventName(this.nameValue, "enable"), {detail: {predicate: this.whenValue, value}});
+      this.dispatchEvent(this.el, signalEventName(this.nameValue, "enable"), {
+        detail: {
+          predicate: this.whenValue,
+          value
+        }
+      });
       this.enable();
     } else {
-      this.dispatchEvent(this.el, signalEventName(this.nameValue, "disable"), {detail: {predicate: this.whenValue, value}});
+      this.dispatchEvent(this.el, signalEventName(this.nameValue, "disable"), {
+        detail: {
+          predicate: this.whenValue,
+          value
+        }
+      });
       this.disable();
     }
   }