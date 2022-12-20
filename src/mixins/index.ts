ea27e1bb19a19c5a8f30641e6b058cc80e29b9c1
Dylan Clarke <Sub-Xaero@users.noreply.github.com>
2022-12-20 02:42:29 +0000

    feat(Mixin): UseIntersection - A mixin that wraps setting up an IntersectionObserver

--- src/mixins/index.ts
+++ src/mixins/index.ts
@@ -2,6 +2,7 @@ export * from './use_event_listener';
 export * from './use_fullscreen';
 export * from './use_geolocation';
 export * from './use_injected_html';
+export * from './use_intersection';
 export * from './use_interval';
 export * from './use_localstorage';
 export * from './use_temporary_content';
