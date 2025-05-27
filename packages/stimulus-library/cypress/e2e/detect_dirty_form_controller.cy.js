858632f9cfdcf784d4145b38e0d259b40886d0fc
John Duff <duff.john@gmail.com>
2025-05-27 12:20:16 -0400

    don't set load value when restoring

--- packages/stimulus-library/cypress/e2e/detect_dirty_form_controller.cy.js
+++ packages/stimulus-library/cypress/e2e/detect_dirty_form_controller.cy.js
@@ -29,4 +29,34 @@ describe("Detect dirty form controller", () => {
     });
 
   });
+
+  it("Should clear the data-dirty attribute when the restore action is triggered", () => {
+    cy.get("form").should("not.have.attr", "data-dirty");
+
+    cy.get("input[type=\"text\"],input[type=\"number\"],input[type=\"checkbox\"],input[type=\"radio\"]:not(:checked),select,textarea").each((el, index, list) => {
+      switch (el[0].tagName) {
+      case "INPUT":
+        if (el[0].type === "checkbox" || el[0].type === "radio") {
+          cy.wrap(el).check();
+        } else {
+          cy.wrap(el).type("123");
+        }
+        break;
+
+      case "TEXTAREA":
+        cy.wrap(el).type("Lorem ipsum");
+        break;
+
+      case "SELECT":
+        cy.wrap(el).select("VOLVO");
+        break;
+      }
+      cy.wrap(el).should("have.attr", "data-dirty");
+      cy.get("form").should("have.attr", "data-dirty");
+      cy.get("#restore-button").click();
+      cy.wrap(el).should("not.have.attr", "data-dirty");
+      cy.get("form").should("not.have.attr", "data-dirty");
+    });
+
+  });
 });
