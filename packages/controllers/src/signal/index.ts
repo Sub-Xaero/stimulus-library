7578a2d9249460222dd352977922ef87d027601f
Dylan Clarke <Sub-Xaero@users.noreply.github.com>
2026-04-07 15:48:24 +0100

    remove SignalTextController (duplicate of SignalContentSyncController)
    Both controllers had identical implementations syncing innerText to a
    signal value. SignalContentSyncController is the canonical version with
    tests and examples registered.

--- packages/controllers/src/signal/index.ts
+++ packages/controllers/src/signal/index.ts
@@ -6,5 +6,4 @@ export * from "./signal_disable_controller";
 export * from "./signal_dom_children_controller";
 export * from "./signal_enable_controller";
 export * from "./signal_input_controller";
-export * from "./signal_text_controller";
 export * from "./signal_visibility_controller";
